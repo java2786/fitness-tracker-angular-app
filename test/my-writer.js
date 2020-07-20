af36762c6790265c6c95edac03b5d38716bb1a08
Arun Kumar <java.kumar.arun@gmail.com>
2020-07-20 18:19:25 +0530

    changes are done

--- test/my-writer.js
+++ test/my-writer.js
@@ -4,7 +4,8 @@ module.exports = {
         let testName = result.fullName.trim(); 
         let fileName = testName.split(" ")[1]; 
 
-        let testNameToCamelCase = testName.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()).replace(/^\w/, c => c.toLowerCase());
+        // let testNameToCamelCase = testName.replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase()).replace(/^\w/, c => c.toLowerCase());
+        let testNameToCamelCase = camelCase(testName);
 
         let fileOutput = `${testNameToCamelCase}=${result.success}`;
         // console.log(fileOutput);
@@ -19,9 +20,10 @@ module.exports = {
 
         // xml.ele('cases', {"xmlns:java": "http://java.sun.com", "xmlns:xsi":"http://www.w3.org/2001/XMLSchema-instance", "xsi:type":"java:com.assessment.data.TestCase"})
         xml.ele('cases')
-        .ele('test-case-type', capitalize(result.fullName.trim().split(" ")[1])).up()
+        // .ele('test-case-type', capitalize(result.fullName.trim().split(" ")[1])).up()
+        .ele('test-case-type', "Functional").up()
         .ele('expected-output', true).up()
-        .ele('name', result.fullName.trim()).up()
+        .ele('name', camelCase(result.fullName.trim())).up()
         .ele('weight', 2).up()
         .ele('mandatory', true).up()
         .ele('desc', "na")
@@ -31,4 +33,12 @@ module.exports = {
 
 const capitalize = function(str){
     return str.charAt(0).toUpperCase()+str.substring(1).toLowerCase();
+}
+
+const camelCase = function(str){
+    var words = str.split(" ").map(word=>{
+        return capitalize(word);
+    });
+    
+    return words.join('').charAt(0).toLowerCase()+words.join('').substring(1);
 }
